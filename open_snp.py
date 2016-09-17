6524d56a9e832406e3e96633e50f510340f1d255
S.P. Mohanty <spmohanty91@gmail.com>
2016-09-17 15:12:13 +0200

    Add training-sepcific functions to the data loader

--- open_snp.py
+++ open_snp.py
@@ -12,12 +12,49 @@ class DataSet:
 	def __init__(self, filename):
 		print "Loading data from : ", filename
 		self._x, self._y = extract_data(filename)
-	def snps():
+		self._epochs_completed = 0
+		self._index_in_epoch = 0
+	
+		assert len(self._x) == len(self._y)
+		self._num_examples = len(self._x)
+	
+	@property
+	def snps(self):
 		return self._x
 
-	def heights():
+	@property
+	def heights(self):
 		return self._y
-				
+	
+	@property
+	def num_examples(self):
+		return self._num_examples
+
+	@property 
+	def epochs_completed(self):
+		return self._epochs_completed
+
+	def next_batch(self, batch_size):
+		start = self._index_in_epoch
+		self._index_in_epoch += batch_size
+		
+		if self._index_in_epoch > self.num_examples:
+			#Finished Epoch
+			self._epochs_completed += 1
+		
+			#Shuffle the data
+			perm = np.arange(self._num_examples)
+			np.random.shuffle(perm)
+			self._x = self._x[perm]
+			self._y = self._y[perm]
+			
+			#Start next epoch
+			start = 0
+			self._index_in_epoch = batch_size
+			assert batch_size <= self._num_examples
+
+		end = self._index_in_epoch
+		return self._x[start:end], self._y[start:end]
 
 def load_data(foldername):
 	return (DataSet(foldername+"/train.h5"), DataSet(foldername+"/test.h5"))
