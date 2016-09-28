Fix small bug in epoch number computation

dnn.py
@@ -180,7 +180,7 @@ with tf.Session(config=config) as sess:
     if ckpt and ckpt.model_checkpoint_path:
         saver.restore(sess, ckpt.model_checkpoint_path)
         epoch_n = int(ckpt.model_checkpoint_path.split('-')[1])
-        i_iter = (epoch_n+1) * (train.num_examples/batch_size)
+        i_iter = (epoch_n) * (train.num_examples/batch_size)
         print "Restored Epoch ", epoch_n
     else:
         if not os.path.exists(CHECKPOINTS):
@@ -192,7 +192,7 @@ with tf.Session(config=config) as sess:
     writer = tf.train.SummaryWriter(LOGDIR, sess.graph_def)
 
     # Training cycle
-    for epoch in range(epoch_n+1, training_epochs):
+    for epoch in range(epoch_n, training_epochs):
         avg_cost = 0.
         total_batch = int(train.num_examples/batch_size)
         # Loop over all batches